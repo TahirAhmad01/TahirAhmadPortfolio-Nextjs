e4e14102c8e3a129a3b7710f60e1a4e53c3354c1
TahirAhmad <tahirahmad0234@gmail.com>
2024-07-10 12:20:32 +0600

    add loading ui

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import "@/assets/css/loading.css";
+import PageLayout from "@/components/PageLayout";
 
 function loading() {
   return (
-    <div className="my-96 text-center">loading</div>
-  )
+    <PageLayout>
+      <div className="my-48 flex justify-center">
+        <div className="parent">
+          <div className="box">
+            <div className="wave" />
+          </div>
+        </div>
+      </div>
+      <svg
+        style={{ visibility: "hidden", position: "absolute" }}
+        width={0}
+        height={0}
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
+      >
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation={15}
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 30 -10"
+              result="goo"
+            />
+            <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+          </filter>
+        </defs>
+      </svg>
+    </PageLayout>
+  );
 }
 
-export default loading
+export default loading;
