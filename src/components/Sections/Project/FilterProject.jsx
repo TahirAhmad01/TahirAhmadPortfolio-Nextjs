64f7e41ff4932722c7f64d21521c0f4990e34809
TahirAhmad <tahirahmad0234@gmail.com>
2024-09-09 10:31:48 +0600

    fix filter with type selection

--- src/components/Sections/Project/FilterProject.jsx
+++ src/components/Sections/Project/FilterProject.jsx
@@ -40,13 +40,7 @@ export function FilterProject({
   };
 
   const handleTypeChange = (type) => {
-    setSelectedTypes((prev) => {
-      if (prev.includes(type)) {
-        return prev.filter((item) => item !== type);
-      } else {
-        return [...prev, type];
-      }
-    });
+    setSelectedTypes([type]); 
   };
 
   return (
@@ -64,63 +58,64 @@ export function FilterProject({
 
         {/* Language Filter */}
         <div className="px-6 overflow-y-auto max-h-[calc(100vh-100px)]">
-        <div className="py-4">
-          <h3 className="text-lg font-semibold">Select Language</h3>
-          <div className="grid gap-4 py-2">
-            {categories.map((category) => {
-              const lowerCaseCategory = category.toLowerCase();
-              const isChecked = selectedCategories.includes(lowerCaseCategory);
+          <div className="py-4">
+            <h3 className="text-lg font-semibold">Select Language</h3>
+            <div className="grid gap-4 py-2">
+              {categories.map((category) => {
+                const lowerCaseCategory = category.toLowerCase();
+                const isChecked =
+                  selectedCategories.includes(lowerCaseCategory);
 
-              return (
-                <div className="flex items-center" key={lowerCaseCategory}>
-                  <Checkbox
-                    id={lowerCaseCategory}
-                    checked={isChecked}
-                    onCheckedChange={() => handleCategoryChange(category)}
-                  />
-                  <Label
-                    htmlFor={lowerCaseCategory}
-                    className="ml-2 text-sm font-medium capitalize"
-                  >
-                    {category}
-                  </Label>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-
-        {/* Project Type Filter */}
-        <div className="py-4">
-          <h3 className="text-lg font-semibold">Project Type</h3>
-          <div className="pt-2">
-            <Checkbox
-              id="Associated"
-              checked={selectedTypes.includes("associated")}
-              onCheckedChange={() => handleTypeChange("associated")}
-            />
-            <Label
-              htmlFor="Associated"
-              className="ml-2 text-sm font-medium capitalize"
-            >
-              Associated with company
-            </Label>
+                return (
+                  <div className="flex items-center" key={lowerCaseCategory}>
+                    <Checkbox
+                      id={lowerCaseCategory}
+                      checked={isChecked}
+                      onCheckedChange={() => handleCategoryChange(category)}
+                    />
+                    <Label
+                      htmlFor={lowerCaseCategory}
+                      className="ml-2 text-sm font-medium capitalize"
+                    >
+                      {category}
+                    </Label>
+                  </div>
+                );
+              })}
+            </div>
           </div>
 
-          <div className="pt-2">
-            <Checkbox
-              id="PersonalProject"
-              checked={selectedTypes.includes("personal")}
-              onCheckedChange={() => handleTypeChange("personal")}
-            />
-            <Label
-              htmlFor="PersonalProject"
-              className="ml-2 text-sm font-medium capitalize"
-            >
-              Personal Project
-            </Label>
+          {/* Project Type Filter */}
+          <div className="py-4">
+            <h3 className="text-lg font-semibold">Project Type</h3>
+            <div className="pt-2">
+              <Checkbox
+                id="Associated"
+                checked={selectedTypes.includes("associated")}
+                onCheckedChange={() => handleTypeChange("associated")}
+              />
+              <Label
+                htmlFor="Associated"
+                className="ml-2 text-sm font-medium capitalize"
+              >
+                Associated with company
+              </Label>
+            </div>
+
+            <div className="pt-2">
+              <Checkbox
+                id="PersonalProject"
+                checked={selectedTypes.includes("personal")}
+                onCheckedChange={() => handleTypeChange("personal")}
+              />
+              <Label
+                htmlFor="PersonalProject"
+                className="ml-2 text-sm font-medium capitalize"
+              >
+                Personal Project
+              </Label>
+            </div>
           </div>
-        </div>
         </div>
       </SheetContent>
     </Sheet>
