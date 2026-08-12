fa8505799418b361af15710317247c771361b2ee
TahirAhmad <tahirahmad0234@gmail.com>
2026-08-12 21:09:14 +0600

    refactor: remove unnecessary fragment wrapper from work experience category tags

--- src/components/Sections/Qualifications/WorkExperience.jsx
+++ src/components/Sections/Qualifications/WorkExperience.jsx
@@ -192,14 +192,12 @@ export default function WorkExperience({ work }) {
                             </div>
 
                             {category.map((cat, idx) => (
-                              <>
-                                <div
-                                  className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300 capitalize md:inline-block hidden shadow-md"
-                                  key={idx}
-                                >
-                                  {cat}
-                                </div>
-                              </>
+                              <div
+                                className="bg-gray-100 text-gray-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-gray-700 dark:text-gray-300 capitalize md:inline-block hidden shadow-md"
+                                key={idx}
+                              >
+                                {cat}
+                              </div>
                             ))}
                           </div>
                         </div>
